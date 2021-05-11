aed32274eff3801d2566c0cd40bb71aa98b6863a
Joey Smeets <joeysmeetsss@gmail.com>
2021-05-11 10:14:33 +0200

    Added the fragments from the assignment to the class.

--- src/ExecutionTime.java
+++ src/ExecutionTime.java
@@ -1,4 +1,65 @@
 
 public class ExecutionTime {
+	
+	public static void main(String[] args) {
+		
+	}
 
+//	// Fragment #1 
+//	for (int i = 0; i < n; i ++) {
+//		sum++;
+//	}
+//	// Fragment #2 
+//	for (int i = 0; i < n; i ++) {
+//	    for ( int j = 0; j < n; j ++) { 
+//	        sum++;
+//	    }
+//	}
+//
+//	// Fragment #3 
+//	for (int i = 0; i < n; i ++) { 
+//	    for ( int j = i; j < n; j ++) {
+//	        sum++;
+//	    }   
+//	}
+//
+//	// Fragment #4 
+//	for (int i = 0; i < n; i ++) {
+//	    sum ++; 
+//	}
+//	for (int j = 0; j < n; j ++) {
+//        sum ++;
+//	}
+//
+//	// Fragment #5 
+//	for (int i = 0; i < n; i ++) {
+//	    for (int j = 0; j < n*n; j ++) {
+//	    sum++;
+//	    }
+//	}
+//
+//	// Fragment #6
+//	for (int i = 0; i < n; i ++) {
+//	    for (int j = 0; j < i; j ++) { 
+//	        sum++;
+//	    }
+//	}
+//
+//	// Fragment #7
+//	for (int i = 1; i < n; i ++) {
+//		for (int j = 0; j < n*n; j ++) {
+//	        if (j % i == 0) {
+//	           for (int k = 0; k < j; k++) {
+//	               sum++;
+//	           }
+//	        }
+//		}
+//	}
+//
+//	// Fragment #8 
+//	int i = n;
+//	while (i > 1) { 
+//	    i = i / 2; 
+//	    sum++; 
+//	}
 }
