2ad2e3a754a10d8406128ec4b2e53116afcde5c3
Joey Smeets <joeysmeetsss@gmail.com>
2021-05-11 13:59:12 +0200

    Further worked on the PrimeCheckerclass. It now holds two ArrayLists with 100 random numbers. These Numbers are checked if they are prime or not. The outcome it printed.

--- src/PrimeChecker.java
+++ src/PrimeChecker.java
@@ -1,27 +1,46 @@
-import java.util.Random;
+import java.util.ArrayList;
 
 public class PrimeChecker {
 	
 	public static void main(String[] args) {
 		
+	//public void FillArrayLists () {
+		ArrayList<Long> numbers20Bits = new ArrayList<>();
+		ArrayList<Long> numbers40Bits = new ArrayList<>();
 		//parts to fill the Array with random 20 or 40 bit numbers. 
-		Random r = new Random();
 		long low = 1;
 		// for 20 Bit numbers
-		long max20 = 1048576;
+		long max20 = 1048576L;
 		// for 40 Bit numbers
-		long max40 = 1099511627776;
-		long result1 = r.nextLong(max20 - low) + low;
-		long result2 = r.nextLong(max40 - low) + low;
-	
-	public static boolean isPrime (Long n) {
-		String b = Long.toBinaryString(n);
-		if (n % 2 == 0) {
-			return true;
-		} 
-		return false;
-
+		long max40 = 1099511627776L;
+		
+		while (numbers20Bits.size() < 100) {
+			long a = (long) (Math.random() * (max20 - low)) + low;
+			if (a > 0) {
+				numbers20Bits.add(a);
+			}
+		}
+		while (numbers40Bits.size() < 100) {
+			long b = (long) (Math.random() * (max40 - low)) + low;
+			if (b > 0) {
+				numbers40Bits.add(b);
+			}
+		}
+		for (int i =0; i<numbers40Bits.size(); i++) {
+			if ((isPrime(numbers40Bits.get(i)))) {
+				System.out.println("The number: " + numbers40Bits.get(i) + "is Prime." );
+			} else {
+				System.out.println("The number: " + numbers40Bits.get(i) + "is not Prime." );
+			}
+		}
 	}
-	
+	;
+	public static boolean isPrime(long n) {
+		for(int i=2; i<=n/2; i++) {
+			if(n%i==0) {return false;}
+		}
+		if(n<2) return false;
+		
+		return true;
 	}
 }
