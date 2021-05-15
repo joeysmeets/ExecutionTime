f90b20c95e4067a5e51adaa62105b6902d6c1b47
Joey Smeets <joeysmeetsss@gmail.com>
2021-05-15 13:44:22 +0200

    Added some changed to the PrimeChecker class.

--- src/ExecutionTime.java
+++ src/ExecutionTime.java
@@ -2,64 +2,67 @@
 public class ExecutionTime {
 	
 	public static void main(String[] args) {
+		long sum = 0;
+		int n = 100000;
 		
-	}
-
-//	// Fragment #1 
-//	for (int i = 0; i < n; i ++) {
-//		sum++;
-//	}
-//	// Fragment #2 
-//	for (int i = 0; i < n; i ++) {
-//	    for ( int j = 0; j < n; j ++) { 
-//	        sum++;
-//	    }
-//	}
-//
-//	// Fragment #3 
-//	for (int i = 0; i < n; i ++) { 
-//	    for ( int j = i; j < n; j ++) {
-//	        sum++;
-//	    }   
-//	}
-//
-//	// Fragment #4 
-//	for (int i = 0; i < n; i ++) {
-//	    sum ++; 
-//	}
-//	for (int j = 0; j < n; j ++) {
-//        sum ++;
-//	}
-//
-//	// Fragment #5 
-//	for (int i = 0; i < n; i ++) {
-//	    for (int j = 0; j < n*n; j ++) {
-//	    sum++;
-//	    }
-//	}
-//
-//	// Fragment #6
-//	for (int i = 0; i < n; i ++) {
-//	    for (int j = 0; j < i; j ++) { 
-//	        sum++;
-//	    }
-//	}
-//
-//	// Fragment #7
-//	for (int i = 1; i < n; i ++) {
-//		for (int j = 0; j < n*n; j ++) {
-//	        if (j % i == 0) {
-//	           for (int k = 0; k < j; k++) {
-//	               sum++;
-//	           }
-//	        }
+//		// Fragment #1  -> 0(N) The operations increase linear to the N-increase.
+//		for (int i = 0; i < n; i ++) {
+//			sum++;
 //		}
-//	}
-//
-//	// Fragment #8 
-//	int i = n;
-//	while (i > 1) { 
-//	    i = i / 2; 
-//	    sum++; 
-//	}
+		
+//		// Fragment #2 --> 0(Nˆ2) The operations increase exponentially once N in
+//		for (int i = 0; i < n; i ++) {
+//		    for ( int j = 0; j < n; j ++) { 
+//		        sum++;
+//		    }
+//		}
+	//
+//		// Fragment #3  --> 
+//		for (int i = 0; i < n; i ++) { 
+//		    for ( int j = i; j < n; j ++) {
+//		        sum++;
+//		    }   
+//		}
+	//
+//		// Fragment #4 --> N * N --> 0(2N) = 0(N) 
+//		for (int i = 0; i < n; i ++) {
+//		    sum ++; 
+//		}
+//		for (int j = 0; j < n; j ++) {
+//	        sum ++;
+//		}
+	
+//		// Fragment #5 
+//		for (int i = 0; i < n; i ++) {
+//		    for (int j = 0; j < n*n; j ++) {
+//		    sum++;
+//		    }
+//		}
+	
+//		// Fragment #6
+//		for (int i = 0; i < n; i ++) {
+//		    for (int j = 0; j < i; j ++) { 
+//		        sum++;
+//		    }
+//		}
+//	
+//		// Fragment #7
+//		for (int i = 1; i < n; i ++) {
+//			for (int j = 0; j < n*n; j ++) {
+//		        if (j % i == 0) {
+//		           for (int k = 0; k < j; k++) {
+//		               sum++;
+//		           }
+//		        }
+//			}
+//		}
+	//
+		// Fragment #8 
+		int i = n;
+		while (i > 1) { 
+		    i = i / 2; 
+		    sum++; 
+		}
+		System.out.println( sum);
+	}
 }
--- src/PrimeChecker.java
+++ src/PrimeChecker.java
@@ -6,13 +6,15 @@ public class PrimeChecker {
 		
 	//public void FillArrayLists () {
 		ArrayList<Long> numbers20Bits = new ArrayList<>();
-		ArrayList<Long> numbers40Bits = new ArrayList<>();
+//		ArrayList<Long> numbers40Bits = new ArrayList<>();
 		//parts to fill the Array with random 20 or 40 bit numbers. 
 		long low = 1;
 		// for 20 Bit numbers
 		long max20 = 1048576L;
 		// for 40 Bit numbers
-		long max40 = 1099511627776L;
+//		long max40 = 1099511627776L;
+		
+		long startTime = System.nanoTime();
 		
 		while (numbers20Bits.size() < 100) {
 			long a = (long) (Math.random() * (max20 - low)) + low;
@@ -20,21 +22,39 @@ public class PrimeChecker {
 				numbers20Bits.add(a);
 			}
 		}
-		while (numbers40Bits.size() < 100) {
-			long b = (long) (Math.random() * (max40 - low)) + low;
-			if (b > 0) {
-				numbers40Bits.add(b);
-			}
-		}
-		for (int i =0; i<numbers40Bits.size(); i++) {
-			if ((isPrime(numbers40Bits.get(i)))) {
-				System.out.println("The number: " + numbers40Bits.get(i) + "is Prime." );
+			
+//		while (numbers40Bits.size() < 100) {
+//			long b = (long) (Math.random() * (max40 - low)) + low;
+//			if (b > 0) {
+//				numbers40Bits.add(b);
+//			}
+//		}	
+		
+		// loop to determine 20Bit prime numbers from the ArrayList
+		for (int i =0; i<numbers20Bits.size(); i++) {
+			if ((isPrime(numbers20Bits.get(i)))) {
+				System.out.println("The number: " + numbers20Bits.get(i) + "is Prime." );
 			} else {
-				System.out.println("The number: " + numbers40Bits.get(i) + "is not Prime." );
+				System.out.println("The number: " + numbers20Bits.get(i) + "is not Prime." );
 			}
 		}
+		long endTime20 = System.nanoTime();
+		long elapedTime = endTime20 - startTime;
+		System.out.println("It took the system " + elapedTime + " Nanoseconds to be done checking all 20 Bit-numbers.");
+		
+		// loop to determine 40Bit prime numbers from the ArrayList
+//		for (int i =0; i<numbers40Bits.size(); i++) {
+//			if ((isPrime(numbers40Bits.get(i)))) {
+//				System.out.println("The number: " + numbers40Bits.get(i) + "is Prime." );
+//			} else {
+//				System.out.println("The number: " + numbers40Bits.get(i) + "is not Prime." );
+//			}
+//		}
+//		long endTime40 = System.nanoTime();
+//		long elapedTime = endTime40 - startTime;
+//		System.out.println("It took the system " + elapedTime + " Nanoseconds to be done checking all 40 Bit-numbers.");
 	}
-	;
+	
 	public static boolean isPrime(long n) {
 		for(int i=2; i<=n/2; i++) {
 			if(n%i==0) {return false;}
